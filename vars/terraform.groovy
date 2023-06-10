edc3495ecd500e56e3a22fb86fabe651649419af
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-06-11 00:22:05 +0530

    frontend

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -15,12 +15,12 @@ def call() {
         stages {
             stage('TERRAFORM INIT') {
                 steps {
-                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                    sh 'terraform init -backend-config=env-dev/state.tfvars'
                 }
             }
             stage('TERRAFORM APPLY') {
                 steps {
-                    sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
+                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
                 }
             }
         }
