2569558ab94552888586c54ab5bf761040c7c1af
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-06-11 23:03:00 +0530

    frontend

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -47,7 +47,7 @@ def call() {
                     sh 'npm install'
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${schema_dir}'
-                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.82.149:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.83.153:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
         }
