13b52a7c14d1869b626e940ed5eaf8bfb968d564
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-06-17 16:19:03 +0530

    frontend

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -10,37 +10,37 @@ def call() {
         options {
             ansiColor('xterm')
         }
-        environment {
-            NEXUS = credentials('NEXUS')
-        }
+
 
         stages {
 
             stage('Code Quality') {
                 steps {
-                //  sh 'ls -l'
-                  //  sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.81.8:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
-                      sh 'echo Code Quality'
+                    //sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.8.27:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                    sh 'echo Code Quality'
                 }
             }
 
             stage('Unit Test Cases') {
                 steps {
-                     sh 'echo Unit Test Cases '
+                    sh 'echo Unit tests'
                     //sh 'npm test'
                 }
             }
+
             stage('CheckMarx SAST Scan') {
                 steps {
-                    sh 'echo CheckMarx SAST Scan '
+                    sh 'echo Checkmarx Scan'
                 }
             }
+
             stage('CheckMarx SCA Scan') {
                 steps {
-                    sh 'echo CheckMarx SCA Scan '
+                    sh 'echo Checkmarx SCA Scan'
                 }
             }
-            stage('Release application') {
+
+            stage('Release Application') {
                 when {
                     expression {
                         env.TAG_NAME ==~ ".*"
@@ -51,26 +51,20 @@ def call() {
                     sh 'zip -r ${component}-${TAG_NAME}.zip *'
                     // Deleting this file as it is not needed.
                     sh 'zip -d ${component}-${TAG_NAME}.zip Jenkinsfile'
-                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.83.153:8081/repository/${component}/${component}-${TAG_NAME}.zip'                }
+                    sh 'curl -f -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.83.153:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                }
             }
+
+
         }
 
         post {
             always {
                 cleanWs()
-             }
+            }
         }
 
     }
 
 
 }
-
-
-
-
-
-
-
-
-
