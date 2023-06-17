36351f3a6af77622188ce6fe9db7eaed297c4544
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-06-17 16:11:38 +0530

    frontend

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -10,6 +10,9 @@ def call() {
         options {
             ansiColor('xterm')
         }
+        environment {
+            NEXUS = credentials('NEXUS')
+        }
 
         stages {
 
@@ -48,8 +51,7 @@ def call() {
                     sh 'zip -r ${component}-${TAG_NAME}.zip *'
                     // Deleting this file as it is not needed.
                     sh 'zip -d ${component}-${TAG_NAME}.zip Jenkinsfile'
-                    sh 'curl -f -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.83.153:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-                }
+                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.83.153:8081/repository/${component}/${component}-${TAG_NAME}.zip'                }
             }
         }
 
